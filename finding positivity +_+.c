311ea8b952bc5fc9c34e65d7d872079756ab7743
Athul-09 <130849190+Athul-09@users.noreply.github.com>
2023-11-01 21:15:49 +0530

    Create finding positivity +_+.c

---
+++ finding positivity +_+.c
@@ -0,0 +1,18 @@
+int firstMissingPositive(int* nums, int numsSize){
+    void swap(int* ay, int* yo) {
+    int temp = *ay;
+    *ay = *yo;
+    *yo = temp;
+}
+for (int i = 0; i < numsSize; i++) {
+        while (nums[i] > 0 && nums[i] <= numsSize && nums[i] != nums[nums[i] - 1]) {
+            swap(&nums[i], &nums[nums[i] - 1]);
+        }
+    }
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] != i + 1) {
+            return i + 1;
+        }
+    }
+    return numsSize + 1;
+}
